fad83636f46ecf6e00948dd88af405a5f1a877be
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2025-07-02 21:04:03 -0300

    feat: create header

---
+++ components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function index() {
+  return (
+    <View>
+      <Text className="text-2xl font-bold text-black-500">Nicolas </Text>
+      <Text className="text-lg text-gray-500">Ferro</Text>
+    </View>
+  );
+}
