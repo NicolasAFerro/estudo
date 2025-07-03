5e2abe73840a4515488bbc25c1b60721967a40f7
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2025-07-02 21:38:09 -0300

    use effect

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/Header";
 
 export default function Index() {
   const [name, setName] = useState<string>("Maria");
+  const [title, setTitle] = useState<string>("React Native");
+
+  //[] montado;/[name] quando atualizar o name/return quando desmontar
+  useEffect(() => {
+    console.log("Component mounted", name);
+  }, []);
+  useEffect(() => {
+    if (title === "React Native") {
+      setTitle("React Native with Expo");
+    } else {
+      setTitle("React Native");
+    }
+    console.log("Title changed", title);
+  }, [name]);
 
   const handlePress = () => {
     setName((old) => {
@@ -13,7 +27,7 @@ export default function Index() {
   };
   return (
     <SafeAreaView className="flex-1 p-5">
-      <Header name={name} title="React Native" />
+      <Header name={name} title={title} />
       <Button title="Change Name" onPress={handlePress} />
     </SafeAreaView>
   );
