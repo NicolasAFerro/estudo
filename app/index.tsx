b4e3c6277f7b49d3d5dead19851a46e1962dc304
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2025-07-02 21:30:18 -0300

    feat: useSTATE

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+import { Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/Header";
 
 export default function Index() {
+  const [name, setName] = useState<string>("Maria");
+
+  const handlePress = () => {
+    setName((old) => {
+      return old === "Maria" ? "Nicolas" : "Maria";
+    });
+  };
   return (
     <SafeAreaView className="flex-1 p-5">
-      <Header name="Nicolas" title="React Native" />
+      <Header name={name} title="React Native" />
+      <Button title="Change Name" onPress={handlePress} />
     </SafeAreaView>
   );
 }
