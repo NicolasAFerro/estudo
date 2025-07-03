2a8065a63c80a6906e2ff9037e13846ab0528a60
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2025-07-02 21:56:22 -0300

    use memo e callback

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/Header";
@@ -20,14 +20,33 @@ export default function Index() {
     console.log("Title changed", title);
   }, [name]);
 
-  const handlePress = () => {
-    setName((old) => {
-      return old === "Maria" ? "Nicolas" : "Maria";
-    });
-  };
+  // const handlePress = useCallback(() => {
+  //   setName((old) => {
+  //     return old === "Maria" ? "Nicolas" : "Maria";
+  //   });
+  // }, []);
+
+  // o aray de dependencia aqui funciona pois se não ele não consegue acessa o array de dependencia na memoria
+  const handlePress = useCallback(() => {
+    if (name === "Maria") {
+      setName("Nicolas");
+    } else {
+      setName("Maria");
+    }
+  }, [name]);
+
+  //use callback;
+  //é uma funçaõ de perfomace, pois toda vez que o componente é montado/rendriza em tela,
+  // ele instancia tudo novamente em memória, ai o useCALLBACK Evita isso, pois a função não precisa
+  // ser recriada toda vez que o componente é renderizado
+
+  //o memo mantém um valor
+  const nameChanged = useMemo(() => {
+    return `${name} | ${new Date().toLocaleTimeString()}`;
+  }, [name]);
   return (
     <SafeAreaView className="flex-1 p-5">
-      <Header name={name} title={title} />
+      <Header name={nameChanged} title={title} />
       <Button title="Change Name" onPress={handlePress} />
     </SafeAreaView>
   );
